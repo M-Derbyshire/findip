a4880b49d6b32cbb5f95f0756d6113181a42fe65
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2026-08-25 20:24:55 +0100

    added IpAddressIsLessThan function

--- ip/compare.go
+++ ip/compare.go
@@ -1,5 +1,10 @@
 package ip
 
+import (
+	"errors"
+	"fmt"
+)
+
 func IpAddressesAreEqual(ip1 IpAddress, ip2 IpAddress) bool {
 	ip1IsNil := ip1 == nil
 	ip2IsNil := ip2 == nil
@@ -12,13 +17,42 @@ func IpAddressesAreEqual(ip1 IpAddress, ip2 IpAddress) bool {
 		return false
 	}
 
-	for i, val1 := range ip1 {
-		val2 := ip2[i]
+	for i, segment1 := range ip1 {
+		segment2 := ip2[i]
 
-		if val1 != val2 {
+		if segment1 != segment2 {
 			return false
 		}
 	}
 
 	return true
+}
+
+func IpAddressIsLessThan(lowerIp IpAddress, higherIp IpAddress) (bool, error) {
+	if lowerIp == nil || higherIp == nil {
+		return false, errors.New("ip addresses must not be nil")
+	}
+
+	if len(lowerIp) != len(higherIp) {
+		lowerStr := convertIpAddressToString(lowerIp)
+		higherStr := convertIpAddressToString(higherIp)
+		return false, fmt.Errorf("ip addresses %s and %s do not have the same amount of segments", lowerStr, higherStr)
+	}
+
+	for i, lowerSegment := range lowerIp {
+		higherSegment := higherIp[i]
+
+		if lowerSegment == higherSegment {
+			continue
+		}
+
+		if lowerSegment < higherSegment {
+			return true, nil
+		}
+
+		return false, nil
+	}
+
+	// all segments must be equal
+	return false, nil
 }
--- ip/compare_test.go
+++ ip/compare_test.go
@@ -117,3 +117,183 @@ func TestIpAddressesAreEqualReturnsFalseIfSegmentsNotEqual(t *testing.T) {
 		})
 	}
 }
+
+func TestIpAddressIsLessThanReturnsTrueIfLessThan(t *testing.T) {
+	tests := []struct {
+		name     string
+		lowerIp  ip.IpAddress
+		higherIp ip.IpAddress
+	}{
+		{
+			name:     "segment-1",
+			lowerIp:  ip.IpAddress{191, 168, 5, 10},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+		{
+			name:     "segment-2",
+			lowerIp:  ip.IpAddress{192, 167, 5, 10},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+		{
+			name:     "segment-3",
+			lowerIp:  ip.IpAddress{192, 168, 4, 10},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+		{
+			name:     "segment-4",
+			lowerIp:  ip.IpAddress{192, 168, 5, 9},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := ip.IpAddressIsLessThan(tt.lowerIp, tt.higherIp)
+
+			if err != nil {
+				t.Errorf("expected error to be nil. got %v", err)
+				return
+			}
+
+			if !result {
+				t.Errorf("expected error to be true. got false")
+			}
+		})
+	}
+}
+
+func TestIpAddressIsLessThanReturnsFalseIfEqual(t *testing.T) {
+	ipAddr := ip.IpAddress{192, 168, 5, 10}
+
+	result, err := ip.IpAddressIsLessThan(ipAddr, ipAddr)
+
+	if err != nil {
+		t.Errorf("expected error to be nil. got %v", err)
+		return
+	}
+
+	if result {
+		t.Errorf("expected result to be false. got true")
+	}
+}
+
+func TestIpAddressIsLessThanReturnsFalseIfGreaterThan(t *testing.T) {
+	tests := []struct {
+		name     string
+		lowerIp  ip.IpAddress
+		higherIp ip.IpAddress
+	}{
+		{
+			name:     "segment-1",
+			lowerIp:  ip.IpAddress{191, 168, 5, 10},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+		{
+			name:     "segment-2",
+			lowerIp:  ip.IpAddress{192, 167, 5, 10},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+		{
+			name:     "segment-3",
+			lowerIp:  ip.IpAddress{192, 168, 4, 10},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+		{
+			name:     "segment-4",
+			lowerIp:  ip.IpAddress{192, 168, 5, 9},
+			higherIp: ip.IpAddress{192, 168, 5, 10},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// parameters passed in the wrong way round
+			result, err := ip.IpAddressIsLessThan(tt.higherIp, tt.lowerIp)
+
+			if err != nil {
+				t.Errorf("expected error to be nil. got %v", err)
+				return
+			}
+
+			if result {
+				t.Errorf("expected result to be false. got true")
+			}
+		})
+	}
+}
+
+func TestIpAddressIsLessThanReturnsErrorIfIpIsNil(t *testing.T) {
+	expectedErr := "ip addresses must not be nil"
+
+	tests := []struct {
+		name     string
+		lowerIp  ip.IpAddress
+		higherIp ip.IpAddress
+	}{
+		{
+			name:     "lower-ip-nil",
+			lowerIp:  nil,
+			higherIp: ip.IpAddress{192, 168, 0, 1},
+		},
+		{
+			name:     "higher-ip-nil",
+			lowerIp:  ip.IpAddress{192, 168, 0, 1},
+			higherIp: nil,
+		},
+		{
+			name:     "both-ip-nil",
+			lowerIp:  nil,
+			higherIp: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := ip.IpAddressIsLessThan(tt.lowerIp, tt.higherIp)
+
+			if err == nil {
+				t.Errorf("expected error to be '%s'. got nil", expectedErr)
+				return
+			}
+
+			if err.Error() != expectedErr {
+				t.Errorf("expected error to be '%s'. got '%v'", expectedErr, err)
+			}
+		})
+	}
+}
+
+func TestIpAddressIsLessThanReturnsErrorIfSegmentLengthsDiffer(t *testing.T) {
+	tests := []struct {
+		name        string
+		lowerIp     ip.IpAddress
+		higherIp    ip.IpAddress
+		expectedErr string
+	}{
+		{
+			name:        "lower-ip-shorter",
+			lowerIp:     ip.IpAddress{192, 168, 1},
+			higherIp:    ip.IpAddress{192, 168, 0, 1},
+			expectedErr: "ip addresses 192.168.1 and 192.168.0.1 do not have the same amount of segments",
+		},
+		{
+			name:        "higher-ip-shorter",
+			lowerIp:     ip.IpAddress{192, 168, 0, 1},
+			higherIp:    ip.IpAddress{192, 168, 1},
+			expectedErr: "ip addresses 192.168.0.1 and 192.168.1 do not have the same amount of segments",
+		},
+	}
+
+	for _, tt := range tests {
+		_, err := ip.IpAddressIsLessThan(tt.lowerIp, tt.higherIp)
+
+		if err == nil {
+			t.Errorf("expected error to be '%s'. got nil", tt.expectedErr)
+			return
+		}
+
+		if err.Error() != tt.expectedErr {
+			t.Errorf("expected error to be '%s'. got '%v'", tt.expectedErr, err)
+		}
+	}
+}
