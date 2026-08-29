58a48d65fdef04fd26ad9a0c8205a8db01baf005
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2026-08-29 13:35:43 +0100

    setup main

--- findip.go
+++ findip.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"findip/config"
+	"findip/scan"
 	"fmt"
 	"os"
 )
 
 func main() {
-	config, err := config.ParseConfigFromFlags()
+	conf, err := config.ParseConfigFromFlags()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Printf("From IP: %d.%d.%d.%d\n", config.FromIp[0], config.FromIp[1], config.FromIp[2], config.FromIp[3])
-	fmt.Printf("To IP: %d.%d.%d.%d\n", config.ToIp[0], config.ToIp[1], config.ToIp[2], config.ToIp[3])
-	fmt.Printf("Port: %d\n", config.Port)
-	fmt.Printf("Endpoint: '%s'\n", config.Endpoint)
-	fmt.Printf("Concurrent requests: %d\n", config.BatchSize)
-	fmt.Printf("Search Text: '%s'\n", config.TextToFind)
+	resultIp := scan.RunIpScan(conf)
+
+	if resultIp == "" {
+		fmt.Fprintln(os.Stderr, "unable to find ip")
+		return
+	}
+
+	fmt.Println(resultIp)
 }
