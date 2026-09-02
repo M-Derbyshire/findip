04de4ec1c45a3715c97fdb8cc3d376f80d8b757e
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2026-09-02 22:00:07 +0100

    added e2e test for ip not found message

--- e2e/error_test.go
+++ e2e/error_test.go
@@ -28,3 +28,27 @@ func TestProgramPrintsInvalidConfigErrorWithCorrectStatusCode(t *testing.T) {
 		t.Errorf("expected exit code to be %d. got %d", expectedExitCode, exitStatus)
 	}
 }
+
+func TestProgramPrintsIpNotFoundErrorWithCorrectStatusCode(t *testing.T) {
+	output, errOutput, exitStatus, err := runProgram("-from", "0.0.0.0", "-to", "0.0.0.0")
+	if err != nil {
+		t.Errorf("error while running command: %v", err.Error())
+		return
+	}
+
+	if len(output) > 0 {
+		t.Errorf("expected no output on stdout. got '%s'", output)
+	}
+
+	expectedErrText := "unable to find ip"
+
+	if !strings.Contains(errOutput, expectedErrText) {
+		t.Errorf("expected error output to contain '%v'. got '%v'", expectedErrText, errOutput)
+	}
+
+	expectedExitCode := 0
+
+	if exitStatus != expectedExitCode {
+		t.Errorf("expected exit code to be %d. got %d", expectedExitCode, exitStatus)
+	}
+}
